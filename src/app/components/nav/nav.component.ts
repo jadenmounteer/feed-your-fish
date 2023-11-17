2e03a23a30f469ce91c668d76a2763cbd820ab0b
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-17 16:38:11 -0700

    Listen to auth status

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { IconService } from 'src/app/services/icon.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy {
   protected isMenuCollapsed: boolean = true;
+  private authSubscription!: Subscription;
+  public isAuth: boolean = false;
+
+  constructor(public icon: IconService, public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 }
