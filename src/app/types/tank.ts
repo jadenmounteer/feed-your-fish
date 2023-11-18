fbfeacf8ed1b818d51f045e9f9494e0e4f8726ac
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-17 17:05:27 -0700

    More type updates

--- src/app/types/tank.ts
+++ src/app/types/tank.ts
@@ -3,6 +3,7 @@ import { Fish } from './fish';
 export interface Tank {
   id: string;
   collaboratorIds: string[] | undefined;
+  usersWatchingTank: string[] | undefined; // Whoever has an id on this tank will default to watching it
   tankName: string;
   fishes: Fish[] | undefined;
 }
