75a49d07af58b1cf3348227949a11af1db1cc322
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-18 09:28:50 -0700

    Add loading to landing page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
+  private authSubscription!: Subscription;
+  public loading: boolean = true;
 
+  constructor(protected authService: AuthService) {}
+  ngOnInit(): void {
+    this.authService.iniAuthListener();
+
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.loading = false;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 }
