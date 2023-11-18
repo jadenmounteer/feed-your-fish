d9d8e0b1dbb06294e1d206ff5160f94d2f9a7f01
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-17 17:24:34 -0700

    typescript updates

--- src/app/components/create-tank-modal/create-tank-modal.component.ts
+++ src/app/components/create-tank-modal/create-tank-modal.component.ts
@@ -4,6 +4,7 @@ import { Tank } from 'src/app/types/tank';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/services/auth.service';
 import { TankService } from 'src/app/services/tank.service';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-create-tank-modal',
@@ -13,9 +14,14 @@ import { TankService } from 'src/app/services/tank.service';
 export class CreateTankModalComponent {
   protected newTank: Partial<Tank> = {
     id: '',
+    createdById: this.authService.userId,
     collaboratorIds: [],
     usersWatchingTank: [],
   };
+
+  protected displayErrorMsg: boolean = false;
+  protected errorMessage: string =
+    'Unable to add fish tank. Please reach out to Jaden for help! 😭';
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -23,4 +29,24 @@ export class CreateTankModalComponent {
     private authService: AuthService,
     private tankService: TankService
   ) {}
+
+  protected onCreateTank() {
+    const newTankId = this.angularFirestore.createId();
+
+    this.newTank.createdById = this.authService.userId;
+
+    this.tankService
+      .createTank(this.newTank, newTankId)
+      .pipe(
+        tap((tank) => {
+          this.activeModal.close(tank);
+        }),
+        catchError((err) => {
+          this.displayErrorMsg = true;
+          this.errorMessage = err;
+          return throwError(err);
+        })
+      )
+      .subscribe();
+  }
 }
--- src/app/types/tank.ts
+++ src/app/types/tank.ts
@@ -2,6 +2,7 @@ import { Fish } from './fish';
 
 export interface Tank {
   id: string;
+  createdById: string;
   collaboratorIds: string[];
   usersWatchingTank: string[]; // Whoever has an id on this tank will default to watching it
   tankName: string;
