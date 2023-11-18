c27689cfe73e60965006c7d7cbc4467e6594fb6a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-17 17:37:01 -0700

    Grab tanks

--- src/app/components/tank-info/tank-info.component.ts
+++ src/app/components/tank-info/tank-info.component.ts
@@ -1,21 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Tank } from 'src/app/types/tank';
 import { CreateTankModalComponent } from '../create-tank-modal/create-tank-modal.component';
+import { Subscription } from 'rxjs';
+import { TankService } from 'src/app/services/tank.service';
 
 @Component({
   selector: 'app-tank-info',
   templateUrl: './tank-info.component.html',
   styleUrls: ['./tank-info.component.scss'],
 })
-export class TankInfoComponent {
+export class TankInfoComponent implements OnInit, OnDestroy {
   @Input() userEmail: string | null | undefined;
   @Input() userId: string | undefined;
 
+  protected loading: boolean = true;
   protected tanks: Tank[] = [];
+  private tankSubscription$ = new Subscription();
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private tankService: TankService
+  ) {}
 
+  ngOnInit(): void {
+    this.loadTanks();
+  }
+
+  private loadTanks(): void {
+    this.tankSubscription$ = this.tankService
+      .fetchTanks()
+      .subscribe((tanks: Tank[]) => {
+        this.tanks = tanks;
+        console.log(this.tanks);
+
+        this.loading = false;
+      });
+  }
   protected onCreateTank(): void {
     const modalRef = this.modalService.open(CreateTankModalComponent);
     modalRef.result.then((result) => {
@@ -23,5 +44,9 @@ export class TankInfoComponent {
         this.tanks.push(result);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.tankSubscription$.unsubscribe();
   }
 }
