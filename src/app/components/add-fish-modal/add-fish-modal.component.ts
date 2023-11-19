f1e0c193acff7c69e2fa3d4f4aec24f49956b8a2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-18 21:10:30 -0700

    Add fish type

--- src/app/components/add-fish-modal/add-fish-modal.component.ts
+++ src/app/components/add-fish-modal/add-fish-modal.component.ts
@@ -3,6 +3,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { NgForm } from '@angular/forms';
 import { Tank } from 'src/app/types/tank';
+import { FishType } from 'src/app/types/fish';
 
 @Component({
   selector: 'app-add-fish-modal',
@@ -12,6 +13,8 @@ import { Tank } from 'src/app/types/tank';
 export class AddFishModalComponent {
   @Input() tank!: Tank;
   protected contentLoaded: boolean = false;
+  protected fishChosen: boolean = false;
+  protected fishType: FishType | undefined;
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -20,5 +23,8 @@ export class AddFishModalComponent {
     this.contentLoaded = true;
   }
 
-  protected onSelectFishType(): void {}
+  protected onSelectFishType(fishType: FishType): void {
+    this.fishChosen = true;
+    this.fishType = fishType;
+  }
 }
