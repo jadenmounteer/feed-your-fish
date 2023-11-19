8d55c4206edc25f2fff376c5b166acd0fda9267b
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-18 22:10:37 -0700

    Update tank when adding fish

--- src/app/components/fish-stats-section/fish-stats-section.component.ts
+++ src/app/components/fish-stats-section/fish-stats-section.component.ts
@@ -3,6 +3,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IconService } from 'src/app/services/icon.service';
 import { AddFishModalComponent } from '../add-fish-modal/add-fish-modal.component';
 import { Tank } from 'src/app/types/tank';
+import { TankService } from 'src/app/services/tank.service';
+import { Fish } from 'src/app/types/fish';
 
 @Component({
   selector: 'app-fish-stats-section',
@@ -14,7 +16,8 @@ export class FishStatsSectionComponent {
 
   constructor(
     protected iconService: IconService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private tankService: TankService
   ) {}
 
   protected addFish(): void {
@@ -22,7 +25,9 @@ export class FishStatsSectionComponent {
     modalRef.componentInstance.tank = this.tank;
 
     modalRef.result.then((result) => {
-      if (result === 'Yes') {
+      if (result.fisName) {
+        this.tank.fishes.push(result);
+        this.tankService.tankViewingChanged.next(this.tank);
       }
     });
   }
