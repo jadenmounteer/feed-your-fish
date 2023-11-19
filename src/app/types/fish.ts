35b128b425fe093d04aa6c6151d30738f16e8bff
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-18 20:26:15 -0700

    Add ngSwitch

--- src/app/types/fish.ts
+++ src/app/types/fish.ts
@@ -1,5 +1,18 @@
+import { GoldfishComponent } from '../components/fish/goldfish/goldfish.component';
+
 export interface Fish {
   id: string;
   userId: string | undefined;
   name: string;
+  fishType: FishType;
 }
+
+export type FishType =
+  | 'goldfish'
+  | 'betta'
+  | 'guppy'
+  | 'tetra'
+  | 'catfish'
+  | 'shark'
+  | 'mermaid'
+  | 'octopus';
