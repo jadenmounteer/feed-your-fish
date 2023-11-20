baec3906b026d221143ed3446e9265ed7dfe578e
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-20 11:44:25 -0700

    Add some fish stat bubble styles

--- src/app/components/fish-stats/fish-stats.component.ts
+++ src/app/components/fish-stats/fish-stats.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Fish } from 'src/app/types/fish';
 
 @Component({
   selector: 'app-fish-stats',
   templateUrl: './fish-stats.component.html',
-  styleUrls: ['./fish-stats.component.scss']
+  styleUrls: ['./fish-stats.component.scss'],
 })
 export class FishStatsComponent {
-
+  @Input() fish!: Fish;
 }
