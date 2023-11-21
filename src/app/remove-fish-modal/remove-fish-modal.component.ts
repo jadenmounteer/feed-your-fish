941a8b2e5bbd890c1d0d3f9b7b238709a638ea31
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-21 15:18:08 -0700

    Add initial eulogy logic

--- src/app/remove-fish-modal/remove-fish-modal.component.ts
+++ src/app/remove-fish-modal/remove-fish-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Fish } from '../types/fish';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -7,11 +7,21 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './remove-fish-modal.component.html',
   styleUrls: ['./remove-fish-modal.component.scss'],
 })
-export class RemoveFishModalComponent {
+export class RemoveFishModalComponent implements OnInit {
   @Input() fish!: Fish;
-  protected contentLoaded: boolean = true;
+  protected contentLoaded: boolean = false;
   protected showAlert: boolean = false;
   protected alertMessage: string = '';
+  protected eulogy: string = '';
 
   constructor(protected activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {
+    this.eulogy = this.generateEulogyForDeadFish();
+    this.contentLoaded = true;
+  }
+
+  private generateEulogyForDeadFish(): string {
+    return `Here lies ${this.fish.fishName}. They were a good fish. They will be missed.`;
+  }
 }
