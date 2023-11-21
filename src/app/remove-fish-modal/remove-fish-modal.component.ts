da8f18d4b46b3e1dd0bdc3d044d3edf2dbb19904
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-11-21 15:15:20 -0700

    More modal logic

--- src/app/remove-fish-modal/remove-fish-modal.component.ts
+++ src/app/remove-fish-modal/remove-fish-modal.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Fish } from '../types/fish';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-remove-fish-modal',
   templateUrl: './remove-fish-modal.component.html',
-  styleUrls: ['./remove-fish-modal.component.scss']
+  styleUrls: ['./remove-fish-modal.component.scss'],
 })
 export class RemoveFishModalComponent {
+  @Input() fish!: Fish;
+  protected contentLoaded: boolean = true;
+  protected showAlert: boolean = false;
+  protected alertMessage: string = '';
 
+  constructor(protected activeModal: NgbActiveModal) {}
 }
