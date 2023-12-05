6c912aa84ffea3dce40219ab5a80911c8f1c73d4
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-05 16:59:14 -0700

    Add icon to header

--- src/app/components/fish-tank-header/fish-tank-header.component.ts
+++ src/app/components/fish-tank-header/fish-tank-header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { IconService } from 'src/app/services/icon.service';
 
 @Component({
   selector: 'app-fish-tank-header',
   templateUrl: './fish-tank-header.component.html',
-  styleUrls: ['./fish-tank-header.component.scss']
+  styleUrls: ['./fish-tank-header.component.scss'],
 })
 export class FishTankHeaderComponent {
-
+  constructor(public icon: IconService) {}
 }
